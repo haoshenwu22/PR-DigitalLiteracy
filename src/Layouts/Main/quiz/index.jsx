077495236d0ac8b2a85a288627da8bd08fcbe7b5
Haoshen Wu <95590048+Alanswagxpp@users.noreply.github.com>
2024-06-27 11:00:07 -0400

    Quiz section

---
+++ src/Layouts/Main/quiz/index.jsx
@@ -0,0 +1,16 @@
+export default function QuizLayout() {
+	return (
+		<>
+			<div className="flex  flex-col w-full bg-red-500 py-8">
+				<p className="text-2xl md:text-6xl mb-4">Don’t know where to start?
+				</p>
+				<p className="md:text-4xl ">Don’t know where to start?
+					No worries! Take this quiz to get your personal suggestion!
+				</p>
+			</div>
+			<div>
+				<p>Hello</p>
+			</div>
+		</>
+	)
+}
