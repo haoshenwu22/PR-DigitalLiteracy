88eea778b180b3cf6c52557f7e886a8190179d69
Florian Reihl <59579743+Florian268@users.noreply.github.com>
2024-07-31 15:31:22 -0400

    Fix ESLint warnings in addQuizQuestion and add questionImage input

--- src/Layouts/Main/AddQuizQuestion/AddQuizQuestion.jsx
+++ src/Layouts/Main/AddQuizQuestion/AddQuizQuestion.jsx
@@ -1,28 +1,17 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import addQuizQuestionToFirestore from './addQuizFirestore';
 
 export default function AddQuizQuestions() {
 	const [platform, setPlatform] = useState('');
 	const [difficulty, setDifficulty] = useState('');
-	const [answerImages, setAnswerImages] = useState([]);
-	const [answerVideos, setAnswerVideos] = useState([]);
-	const [questionImages, setQuestionImages] = useState([]);
 	const [id, setId] = useState('');
 	const [answerText, setAnswerText] = useState('');
+	const [answerImages, setAnswerImages] = useState([]);
+	// const [answerVideos, setAnswerVideos] = useState([]);
+	const [questionImages, setQuestionImages] = useState([]);
 	const [questionText, setQuestionText] = useState('');
 	const [options, setOptions] = useState([]);
-
-	const handleOptionChange = (index, field, value) => {
-		setOptions((prevOptions) => prevOptions.map((option, i) => (i === index ? { ...option, [field]: value } : option)));
-	};
-
-	const handleAddOption = () => {
-		setOptions((prevOptions) => [...prevOptions, { label: '', images: [], isCorrect: false }]);
-	};
-
-	const handleRemoveOption = (index) => {
-		setOptions((prevOptions) => prevOptions.filter((_, i) => i !== index));
-	};
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -47,45 +36,72 @@ export default function AddQuizQuestions() {
 			setDifficulty('');
 			setId('');
 
-			setAnswerText('');
-			setAnswerVideos([]);
-			setOptions([]);
-
 			setQuestionText('');
-			setAnswerImages([]);
 			setQuestionImages([]);
+			setAnswerText('');
+			setAnswerImages([]);
+			// setAnswerVideos([]);
+
+			setOptions([]);
 		} catch (error) {
 			console.error('Error adding question:', error);
 		}
 	};
 
-	const handleOptionImageDrop = (e, optionIndex) => {
+	const handleImageDragOver = (e) => {
 		e.preventDefault();
-		const files = Array.from(e.dataTransfer.files);
-
-		setOptions((prevOptions) =>
-			prevOptions.map((option, index) =>
-				index === optionIndex ? { ...option, images: [...option.images, ...files] } : option,
-			),
-		);
 	};
 
 	const handleAnswerImageDrop = (e) => {
 		e.preventDefault();
 		const files = Array.from(e.dataTransfer.files);
 
-		setAnswerImages((prevImages) => [...prevImages, ...files]);
+		const uploadedImages = files.map((file) => ({
+			id: uuidv4(),
+			file,
+		}));
+
+		setAnswerImages((prevImages) => [...prevImages, ...uploadedImages]);
 	};
 
 	const handleQuestionImageDrop = (e) => {
 		e.preventDefault();
 		const files = Array.from(e.dataTransfer.files);
 
-		setQuestionImages((prevImages) => [...prevImages, ...files]);
+		const uploadedImages = files.map((file) => ({
+			id: uuidv4(),
+			file,
+		}));
+
+		setQuestionImages((prevImages) => [...prevImages, ...uploadedImages]);
 	};
 
-	const handleImageDragOver = (e) => {
+	const handleOptionImageDrop = (e, optionIndex) => {
 		e.preventDefault();
+		const files = Array.from(e.dataTransfer.files);
+
+		const uploadedImages = files.map((file) => ({
+			id: uuidv4(),
+			file,
+		}));
+
+		setOptions((prevOptions) =>
+			prevOptions.map((option, index) =>
+				index === optionIndex ? { ...option, images: [...option.images, ...uploadedImages] } : option,
+			),
+		);
+	};
+
+	const handleOptionChange = (index, field, value) => {
+		setOptions((prevOptions) => prevOptions.map((option, i) => (i === index ? { ...option, [field]: value } : option)));
+	};
+
+	const handleAddOption = () => {
+		setOptions((prevOptions) => [...prevOptions, { id: uuidv4(), label: '', images: [], isCorrect: false }]);
+	};
+
+	const handleRemoveOption = (index) => {
+		setOptions((prevOptions) => prevOptions.filter((_, i) => i !== index));
 	};
 
 	return (
@@ -93,59 +109,59 @@ export default function AddQuizQuestions() {
 			<div>
 				<label htmlFor="platform" className="block text-gray-700 font-bold mb-2">
 					Platform:
+					<select
+						id="platform"
+						value={platform}
+						onChange={(e) => setPlatform(e.target.value)}
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					>
+						<option value="">Select Platform</option>
+						<option value="mobile">Mobile</option>
+						<option value="desktop">Desktop</option>
+					</select>
 				</label>
-				<select
-					id="platform"
-					value={platform}
-					onChange={(e) => setPlatform(e.target.value)}
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				>
-					<option value="">Select Platform</option>
-					<option value="mobile">Mobile</option>
-					<option value="desktop">Desktop</option>
-				</select>
 			</div>
 
 			<div>
 				<label htmlFor="difficulty" className="block text-gray-700 font-bold mb-2 mt-6">
 					Difficulty:
+					<select
+						id="difficulty"
+						value={difficulty}
+						onChange={(e) => setDifficulty(e.target.value)}
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					>
+						<option value="">Select Difficulty</option>
+						<option value="easy">Easy</option>
+						<option value="medium">Medium</option>
+						<option value="hard">Hard</option>
+					</select>
 				</label>
-				<select
-					id="difficulty"
-					value={difficulty}
-					onChange={(e) => setDifficulty(e.target.value)}
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				>
-					<option value="">Select Difficulty</option>
-					<option value="easy">Easy</option>
-					<option value="medium">Medium</option>
-					<option value="hard">Hard</option>
-				</select>
 			</div>
 
 			<div>
 				<label htmlFor="id" className="block text-gray-700 font-bold mb-2 mt-6">
 					ID:
+					<input
+						type="text"
+						id="id"
+						value={id}
+						onChange={(e) => setId(e.target.value)}
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+					/>
 				</label>
-				<input
-					type="text"
-					id="id"
-					value={id}
-					onChange={(e) => setId(e.target.value)}
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-				/>
 			</div>
 
 			<div>
 				<label htmlFor="text" className="block text-gray-700 font-bold mb-2 mt-6">
 					Question Text:
+					<textarea
+						id="text"
+						value={questionText}
+						onChange={(e) => setQuestionText(e.target.value)}
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20"
+					/>
 				</label>
-				<textarea
-					id="text"
-					value={questionText}
-					onChange={(e) => setQuestionText(e.target.value)}
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20"
-				/>
 			</div>
 
 			<div
@@ -157,9 +173,9 @@ export default function AddQuizQuestions() {
 				<div className="flex flex-wrap">
 					{questionImages.map((image, index) => (
 						<img
-							key={index}
-							src={URL.createObjectURL(image)}
-							alt={`Question Image ${index}`}
+							key={image.id}
+							src={URL.createObjectURL(image.file)}
+							alt={`Question Index ${index}`}
 							className="max-w-xs h-auto mr-2 mb-2 rounded-md"
 						/>
 					))}
@@ -169,14 +185,14 @@ export default function AddQuizQuestions() {
 			<div>
 				<label htmlFor="answerText" className="block text-gray-700 font-bold mb-2 mt-6">
 					Answer Text:
+					<textarea
+						type="text"
+						id="answerText"
+						value={answerText}
+						onChange={(e) => setAnswerText(e.target.value)}
+						className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20"
+					/>
 				</label>
-				<textarea
-					type="text"
-					id="answerText"
-					value={answerText}
-					onChange={(e) => setAnswerText(e.target.value)}
-					className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline h-20"
-				/>
 			</div>
 
 			<div
@@ -188,9 +204,9 @@ export default function AddQuizQuestions() {
 				<div className="flex flex-wrap">
 					{answerImages.map((image, index) => (
 						<img
-							key={index}
-							src={URL.createObjectURL(image)}
-							alt={`Answer Image ${index}`}
+							key={image.id}
+							src={URL.createObjectURL(image.file)}
+							alt={`Answer Index ${index}`}
 							className="max-w-xs h-auto mr-2 mb-2 rounded-md"
 						/>
 					))}
@@ -198,33 +214,33 @@ export default function AddQuizQuestions() {
 			</div>
 
 			<h3 className="text-lg font-bold mb-4 mt-6">Options:</h3>
-			{options.map((option, index) => (
-				<div key={index} className="mb-4 border rounded-md p-4 shadow-sm">
+			{options.map((option, optionIndex) => (
+				<div key={option.id} className="mb-4 border rounded-md p-4 shadow-sm">
 					<div className="mb-2">
-						<label htmlFor={`optionLabel${index}`} className="block text-gray-700 font-bold mb-2">
+						<label htmlFor={`optionLabel${optionIndex}`} className="block text-gray-700 font-bold mb-2">
 							Label:
+							<input
+								type="text"
+								id={`optionLabel${optionIndex}`}
+								value={option.label}
+								onChange={(e) => handleOptionChange(optionIndex, 'label', e.target.value)}
+								className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+							/>
 						</label>
-						<input
-							type="text"
-							id={`optionLabel${index}`}
-							value={option.label}
-							onChange={(e) => handleOptionChange(index, 'label', e.target.value)}
-							className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-						/>
 					</div>
 
-					<div // Drag-and-drop area for option images
-						onDrop={(e) => handleOptionImageDrop(e, index)}
+					<div
+						onDrop={(e) => handleOptionImageDrop(e, optionIndex)}
 						className="mb-2 border-2 border-dashed border-gray-300 p-4 flex flex-wrap cursor-pointer"
 					>
 						<div className="mb-2">
-							<p className="text-gray-500">Drag & Drop Images for Option {index + 1}</p>
+							<p className="text-gray-500">Drag & Drop Images for Option {optionIndex + 1}</p>
 							<div className="flex flex-wrap">
 								{option.images.map((image, imgIndex) => (
 									<img
-										key={imgIndex}
-										src={URL.createObjectURL(image)}
-										alt={`Option ${index + 1} Image ${imgIndex}`}
+										key={image.id}
+										src={URL.createObjectURL(image.file)}
+										alt={`Option ${optionIndex + 1} Index ${imgIndex}`}
 										className="max-w-xs h-auto mr-2 mb-2 rounded-md"
 									/>
 								))}
@@ -233,20 +249,20 @@ export default function AddQuizQuestions() {
 					</div>
 
 					<div className="flex items-center mb-2">
-						<input
-							type="checkbox"
-							id={`optionIsCorrect${index}`}
-							checked={option.isCorrect}
-							onChange={(e) => handleOptionChange(index, 'isCorrect', e.target.checked)}
-							className="mr-2 leading-tight"
-						/>
-						<label htmlFor={`optionIsCorrect${index}`} className="text-gray-700">
+						<label htmlFor={`optionIsCorrect${optionIndex}`} className="text-gray-700">
 							Is Correct
+							<input
+								type="checkbox"
+								id={`optionIsCorrect${optionIndex}`}
+								checked={option.isCorrect}
+								onChange={(e) => handleOptionChange(optionIndex, 'isCorrect', e.target.checked)}
+								className="ml-2 leading-tight"
+							/>
 						</label>
 					</div>
 					<button
 						type="button"
-						onClick={() => handleRemoveOption(index)}
+						onClick={() => handleRemoveOption(optionIndex)}
 						className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
 					>
 						Remove Option
--- src/Layouts/Main/AddQuizQuestion/addQuizFirestore.js
+++ src/Layouts/Main/AddQuizQuestion/addQuizFirestore.js
@@ -1,5 +1,4 @@
-import { getFirestore, collection, doc, setDoc, addDoc, getDoc, query, getDocs, where } from 'firebase/firestore';
-import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { collection, doc, setDoc, getDoc, getDocs } from 'firebase/firestore';
 import { db } from '../../../firebase/firebase';
 
 export default async function addQuizQuestionToFirestore(questionData) {
@@ -14,10 +13,10 @@ export default async function addQuizQuestionToFirestore(questionData) {
 		// Convert main question images to Base64
 		firestoreData.questionImages = await Promise.all(
 			questionData.questionImages.map(
-				async (imageFile) =>
+				async (image) =>
 					new Promise((resolve, reject) => {
 						const reader = new FileReader();
-						reader.readAsDataURL(imageFile);
+						reader.readAsDataURL(image.file);
 						reader.onload = () => resolve(reader.result);
 						reader.onerror = (error) => reject(error);
 					}),
@@ -27,10 +26,10 @@ export default async function addQuizQuestionToFirestore(questionData) {
 		// Convert main answer images to Base64
 		firestoreData.answerImages = await Promise.all(
 			questionData.answerImages.map(
-				async (imageFile) =>
+				async (image) =>
 					new Promise((resolve, reject) => {
 						const reader = new FileReader();
-						reader.readAsDataURL(imageFile);
+						reader.readAsDataURL(image.file);
 						reader.onload = () => resolve(reader.result);
 						reader.onerror = (error) => reject(error);
 					}),
@@ -42,10 +41,10 @@ export default async function addQuizQuestionToFirestore(questionData) {
 			questionData.options.map(async (option) => {
 				const optionImages = await Promise.all(
 					option.images.map(
-						async (imageFile) =>
+						async (image) =>
 							new Promise((resolve, reject) => {
 								const reader = new FileReader();
-								reader.readAsDataURL(imageFile);
+								reader.readAsDataURL(image.file);
 								reader.onload = () => resolve(reader.result);
 								reader.onerror = (error) => reject(error);
 							}),
@@ -86,19 +85,19 @@ export async function retrieveAllQuestionsForPlatform(platform) {
 			hard: [],
 		};
 
-		for (const difficultyDoc of difficultySnapshot.docs) {
+		difficultySnapshot.docs.forEach(async (difficultyDoc) => {
 			const difficultyId = difficultyDoc.id;
 			const questionsCollectionRef = collection(difficultyDoc.ref, 'questions');
 			const questionsSnapshot = await getDocs(questionsCollectionRef);
 
-			for (const questionDoc of questionsSnapshot.docs) {
+			questionsSnapshot.docs.forEach((questionDoc) => {
 				const questionData = questionDoc.data();
 				questionsByDifficulty[difficultyId].push({
 					...questionData,
 					id: questionDoc.id,
 				});
-			}
-		}
+			});
+		});
 
 		return { [platform]: questionsByDifficulty };
 	} catch (error) {
